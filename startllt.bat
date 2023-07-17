bebcd013fa30ef2e8dc456f7e5c757f87def46da
Ipgmc <131883119+Ipgmc@users.noreply.github.com>
2023-07-17 16:23:21 +0200

    Add files via upload

---
+++ startllt.bat
@@ -0,0 +1,4 @@
+@echo off 
+cls 
+start "" "C:\Users\User\AppData\Local\Programs\Python\Python310\python.exe" "main.py" 
+exit 
---
+++ tst.py
@@ -0,0 +1,9 @@
+from colorama import *
+init(autoreset=True)
+
+print(Fore.YELLOW + 'Ein Gelber Text')
+
+print(Fore.YELLOW + Back.RED + 'Ein Gelber Text mit Rotem Hintergrund')
+
+print("Normaler Text") 
+input()
